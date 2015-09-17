0aef32673644c1a0ed0c28fe27f30c797d658d9e
Travis Dilweg <travis.dilweg@gmail.com>
2015-09-17 19:47:54 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NickRay::Application.config.secret_key_base = '4b921927c2b9a2c005e221111ba1cd5ac7c13ac5f731a6322dcee6d24b65d1862e895af70e7c05f33b5fe99c03e0bb521fa684c302a8dfb8c1644d5c2993ab0c'
