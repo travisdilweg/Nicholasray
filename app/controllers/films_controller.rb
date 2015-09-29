aa91eb76186d48640f657ff0fc6866371d9fbf0a
Travis Dilweg <travis.dilweg@gmail.com>
2015-09-29 11:44:47 +0000

    Add film form/functionality added

---
+++ app/controllers/films_controller.rb
@@ -0,0 +1,10 @@
+class FilmsController < ApplicationController
+	def index
+		@films = Film.all
+	end
+
+	def new
+		@film = Film.new
+	end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 NickRay::Application.routes.draw do
-  root 'notes#index'
+  root 'films#index'
+
+  resources :films
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
